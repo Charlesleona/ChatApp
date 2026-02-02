100ad5c5cec95975e568f6a8e52aa118d5a6ca57
Charles M <charlesmariajoesph@gmail.com>
2026-02-02 21:50:02 +0530

    untill bottom of chat app

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -27,7 +27,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
       </div>
       {/* chat area  */}
-      <div className="flex flex-col h-[cal(100%-120px)] overflow-y-scroll p-3 pb-b6">
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
         {messagesDummyData.map((msg, index) => (
           <div
             key={index}
@@ -64,9 +64,28 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         ))}
         <div ref={scrollEnd}></div>
       </div>
+      {/* Bottom area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              alt=""
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+      </div>
     </div>
   ) : (
-    <div className="flex flex-col items-center justify-center gap-2 text-gray-500">
+    <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
       <img src={assets.logo_icon} alt="" className="max-w-16" />
       <p className="text-lg font-medium text-white">Chat anytime, anywhere </p>
     </div>
@@ -74,5 +93,3 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
 };
 
 export default ChatContainer;
-
-// 1:14
