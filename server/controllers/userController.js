cdaa851890932dd05cdb9805fcae1ea0229351ae
Charles M <charlesmariajoesph@gmail.com>
2026-02-12 21:28:10 +0530

    controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import { TokenExpiredError } from "jsonwebtoken";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 // Signup new user
 export const signup = async (req, res) => {
@@ -57,6 +58,41 @@ export const login = async (req, res) => {
     message: "Login successfully",
   });
   try {
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res) => {
+  res.json({ success: true, user: req.user });
+};
+
+// Controller to update user profile details
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic, bio, fullName } = req.body;
+    const userId = req.user._id;
+    let updatedUser;
+    if (!profilePic) {
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { bio, fullName },
+        { new: true },
+      );
+    } else {
+      const upload = await cloudinary.uploader.upload(profilePic);
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { profilePic: upload.secure_url, bio, fullName },
+        { new: true },
+      );
+    }
+    res.json({ success: true, user: updatedUser });
   } catch (error) {
     console.log(error.message);
     res.json({
