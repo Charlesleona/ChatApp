a37c053191ce321f48aa9c8a62a9be36d8a4e85d
Charles M <charlesmariajoesph@gmail.com>
2026-02-12 22:09:41 +0530

    message get controller

---
+++ server/controllers/messageController.js
@@ -0,0 +1,33 @@
+// Get all users expect the logged in user
+
+import Message from "../models/Message.js";
+import User from "../models/user.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUser = await User.find({ _id: { $ne: userId } }).select(
+      "-password",
+    );
+    // count number of messages not seen
+    const unseenMessage = {};
+    const promises = filteredUser.map(async (user) => {
+      const message = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (message.length > 0) {
+        unseenMessage[user._id] = message.length;
+      }
+    });
+    await promises.all(promises);
+    res.json({ success: true, user: filteredUser, unseenMessage });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDb } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // create Express app and HTTP server
 const app = express();
@@ -12,11 +13,12 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-// Routes
+// Routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 // Connect to MongoDB
-await connectDb()
+await connectDb();
 
 const PORT = process.env.port || 5000;
 server.listen(PORT, () => {
