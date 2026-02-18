2dc607e0fd31a26455451cec9e9dedd47e8eb458
Charles M <charlesmariajoesph@gmail.com>
2026-02-18 22:04:37 +0530

    frontend basic auth set up

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [authUser, setAuthUser] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [socket, setSocket] = useState(null);
+  // check if user is authenticated nd if so, set the user data and connect the socket
+
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/check");
+      if (data.success) {
+        setAuthUser(data.user);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["token"] = token;
+    }
+      checkAuth();
+  }, []);
+
+  const value = {
+    axios,
+    authUser,
+    onlineUsers,
+    socket,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
@@ -9,11 +10,13 @@ const App = () => {
       className="bg-[url('./src/assets/bgImage.svg')]
     bg-contain"
     >
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
+      <Toaster>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Toaster>
     </div>
   );
 };
