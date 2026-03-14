23fd947300c3cd594432e629211de2f5d40186d3
Charles M <charlesmariajoesph@gmail.com>
2026-03-14 17:10:09 +0530

    remove .DS_Store

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use("/api/message", messageRouter);
 // Connect to MongoDB
 await connectDb();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "Production") {
   const PORT = process.env.port || 5000;
   server.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
